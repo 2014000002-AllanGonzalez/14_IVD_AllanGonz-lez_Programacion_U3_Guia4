1d337a0ab61f506285e0a85266e8de71b22feb71
2014000002-AllanGonzalez <a.gonzalez.5ddb@gmail.com>
2026-07-22 20:06:29 -0600

    C#

--- Lecc34_Act3.cs
+++ Lecc34_Act3.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double celsius, resultado;
+        int opcion;
+
+        Console.Write("Ingrese la temperatura en grados Celsius: ");
+        celsius = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\nSeleccione una opción:");
+        Console.WriteLine("1. Convertir a Fahrenheit");
+        Console.WriteLine("2. Convertir a Kelvin");
+        Console.Write("Opción: ");
+        opcion = Convert.ToInt32(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                resultado = (celsius * 9 / 5) + 32;
+                Console.WriteLine("Temperatura en Fahrenheit: " + resultado);
+                break;
+
+            case 2:
+                resultado = celsius + 273.15;
+                Console.WriteLine("Temperatura en Kelvin: " + resultado);
+                break;
+
+            default:
+                Console.WriteLine("Opción inválida.");
+                break;
+        }
+    }
+}
